17eeabc6b6bdd57d41824c8a1d78af2d27dc3faa
tukars <183424959+tukarsdev@users.noreply.github.com>
2025-11-06 22:39:57 +0000

    Add support for css and js static files

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,9 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addNunjucksShortcode("year", function () {
 		return new Date().getFullYear();
 	});
+  
+  eleventyConfig.addPassthroughCopy({ "src/dev/tukars/styles": "styles" });
+  eleventyConfig.addPassthroughCopy({ "src/dev/tukars/scripts": "scripts" });
 	
   return {
     dir: {
